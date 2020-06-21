4402f8d57f72f20563f315f8d9797a8118ed34ed
Victor Mesquita <mesquitadev@gmail.com>
2020-06-21 14:19:30 -0300

    M - Initial

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "#######################################################"
+echo " Script de Instalação e configuração do ambiente Linux "
+echo "    developed by: Victor Mesquita (@mesquitadev)       "
+echo "                    Versão: 0.0.1                      "
+echo "#######################################################"
