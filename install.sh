894d12ad3c5e7d272755ca78ee49c7f547eb9247
Victor Mesquita <mesquitadev@gmail.com>
2020-06-24 09:26:23 -0300

    M - Iniciando Verificações

--- install.sh
+++ install.sh
@@ -1,25 +1,55 @@
 #!/usr/bin/env bash
 
-echo "#######################################################"
-echo " Script de Instalação e configuração do ambiente Linux "
-echo "    developed by: Victor Mesquita (@mesquitadev)       "
-echo "                    Versão: 0.0.1                      "
-echo "#######################################################"
 
 USUARIO=`id -u`
 UBUNTU=`lsb_release -rs`
 KERNEL=`uname -r | cut -d'.' -f1,2`
 LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
 
+clear
 if [ "$USUARIO" -eq "0" ] && (( $(echo "$KERNEL >= 4.15" |bc -l) ));
 	then
 		echo -e "O usuário e Root, continuando com o script..."
-		echo -e "Distribuição e >=18.04.x, continuando com o script..."
 		echo -e "Kernel e >= 4.15, continuando com o script..."
 		sleep 5
 	else
-		echo -e "Usuário não e Root ($USUARIO) ou Distribuição não e >=18.04.x ($UBUNTU) ou Kernel não e >=4.15 ($KERNEL)"
+		echo -e "Usuário não e Root ($USUARIO) ou Kernel não e >=4.15 ($KERNEL)"
 		echo -e "Caso você não tenha executado o script com o comando: sudo -i"
 		echo -e "Execute novamente o script para verificar o ambiente."
 		exit 1
 fi
+echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
+clear
+echo "#######################################################"
+echo " Script de Instalação e configuração do ambiente Linux "
+echo "    developed by: Victor Mesquita (@mesquitadev)       "
+echo "                    Versão: 0.0.1                      "
+echo "#######################################################"
+
+echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet..."
+sleep 5
+echo
+#
+echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
+  add-apt-repository universe &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Atualizando as listas do Apt, aguarde..."
+  apt update &>> $LOG
+echo -e "Listas atualizadas com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Atualizando o sistema, aguarde..."
+	apt -y upgrade &>> $LOG
+echo -e "Sistema atualizado com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Removendo software desnecessários, aguarde..."
+	apt -y autoremove &>> $LOG
+echo -e "Software removidos com sucesso!!!, continuando com o script..."
+sleep 5
+echo
